b2a4804b39afc4ef637b0b8ec5fa28d1f8e2a984
Jake <monkeypsycho03@gmail.com>
2020-09-08 13:33:40 -0600

    structure being setup
    For this example the data we will be working on will be types of account and we will do all sorts of things with them see: https://levelup.gitconnected.com/functional-object-oriented-procedural-programming-644feda5bcfc for examples on what i will do. the borrow checker might disagree about what i am doing but thats the checkers problem not mine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+
 main(){
     println!("hello world");
+    
+    // we might have to use an rr<cell<HashMap<String>, <i32>> to make this work
+    let accounts: Vec<HashMap<String>, <i32>> = HashMap::new();
+}
+
+fn get_account(name: String){
+    for account in accounts{
+        match account.get(name){
+            Some(acc) => return acc,
+            None => panic!("account dont exist")
+        }
+    }
 }
