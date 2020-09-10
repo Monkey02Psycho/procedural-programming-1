8d7a20cf4fdcf9c73bd59d5d95523e4e59c74f49
Jake <monkeypsycho03@gmail.com>
2020-09-10 13:41:22 -0600

    creating procedural style in C because thats all C allows

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct Vec{
+    int x;
+    int y;
+} typedef Vec;
+int main()
+{
+    Vec test;
+    test.x = 5;
+    test.y =10;
+    printf("The integer is: %d\n", test.x);
+
+    return 0;
+}
+
+Vec add(Vec, Vec){
+    
+}    
