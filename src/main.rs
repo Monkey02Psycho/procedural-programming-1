b5560115e486b00a839d12adb3a685ced5e2d83d
Jake <monkeypsycho03@gmail.com>
2020-09-10 11:36:12 -0600

    properly implemented get_account

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::collections::HashMap;
 
-main(){
-    println!("hello world");
+fn main(){
     
     // we might have to use an rr<cell<HashMap<String>, <i32>> to make this work
-    let accounts: Vec<HashMap<String>, <i32>> = HashMap::new();
+    let mut accounts: Vec<HashMap<String, i32>> = vec![HashMap::new()];
+    
+    accounts[0].insert("1".to_string(), 542);
+    accounts[0].insert("2".to_string(), 431);
+    
+    println!("{}", accounts[0].get(&"1".to_string()).unwrap());
+    
+    get_account("2".to_string(), &accounts)
+    
 }
 
-fn get_account(name: String){
+fn get_account(name: String, accounts: &Vec<HashMap<String, i32>>){
     for account in accounts{
-        match account.get(name){
-            Some(acc) => return acc,
-            None => panic!("account dont exist")
+        match account.get(&name){
+            Some(acc) => println!("{}", acc),
+            None => panic!("account dont exist") // should we panic or just return Result<(), Error>?
         }
     }
 }
