5f667b247e075be098cd965800145d2c740be785
Jake <monkeypsycho03@gmail.com>
2020-09-10 13:29:42 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
-
+// We def should not use RUST
 fn main(){
     
-    // we might have to use an rr<cell<HashMap<String>, <i32>> to make this work
+    // we might have to use an rc<cell<HashMap<String>, <i32>> to make this work
     let mut accounts: Vec<HashMap<String, i32>> = vec![HashMap::new()];
     
     accounts[0].insert("1".to_string(), 542);
@@ -10,15 +10,18 @@ fn main(){
     
     println!("{}", accounts[0].get(&"1".to_string()).unwrap());
     
-    get_account("2".to_string(), &accounts)
+    println!("{:?}",get_account("2".to_string(), &accounts))
     
 }
 
-fn get_account(name: String, accounts: &Vec<HashMap<String, i32>>){
+fn get_account(name: String, accounts: &mut Vec<HashMap<String, i32>>) -> &HashMap<String, i32>{
     for account in accounts{
         match account.get(&name){
-            Some(acc) => println!("{}", acc),
-            None => panic!("account dont exist") // should we panic or just return Result<(), Error>?
+            Some(acc) => account,
+            None => panic!("account dont exist"); // should we panic or just return Result<(), Error>?
         }
     }
+}
+
+fn deposit(name: String, amount: i32, &Vec<HashMap<String, i32>>){
 }
