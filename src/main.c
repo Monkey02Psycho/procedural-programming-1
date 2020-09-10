24792e378015ee0473a385edff70b098bafcaede
Jake <monkeypsycho03@gmail.com>
2020-09-10 13:53:13 -0600

    now you can add

--- src/main.c
+++ src/main.c
@@ -3,16 +3,26 @@ struct Vec{
     int x;
     int y;
 } typedef Vec;
+Vec add(Vec first, Vec right);
 int main()
 {
     Vec test;
+    Vec test2;
     test.x = 5;
     test.y =10;
+    test2.x = 5;
+    test2.y =10;
     printf("The integer is: %d\n", test.x);
-
+    Vec new;
+    new = add(test, test2);
+    printf("The integer is: %d\n", new.x);
     return 0;
 }
 
-Vec add(Vec, Vec){
+Vec add(Vec first, Vec right){
+    Vec new;
+    new.x = first.x + right.x;
+    new.y = first.x + right.x;
+    return new;
     
-}    
+}   
