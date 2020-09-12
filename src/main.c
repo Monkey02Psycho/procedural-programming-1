f7fa175459151ac964a6e6613bbdb9182b3eef34
Jake <monkeypsycho03@gmail.com>
2020-09-12 16:39:35 -0600

    added magnitude calcuation

--- src/main.c
+++ src/main.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
- 
+#include <math.h>
+
 struct Vec{
-	float x;
-	float y;
+	double x;
+	double y;
 } typedef Vec;
- 
+
 Vec add(Vec first, Vec second){
     Vec new;
     new.x = first.x + second.x; 
     new.y = first.y + second.y;
     return new;
 }
- 
+
 Vec sub(Vec first, Vec second){
     Vec new;
     new.x = first.x - second.x; 
     new.y = first.y -  second.y;
     return new;
 }
- 
-Vec mul(Vec first, float second){
+
+Vec mul(Vec first, double second){
     Vec new;
     new.x = first.x * second; 
     new.y = first.y *  second;
     return new;
 }
-Vec div(Vec first, float second){
+Vec div(Vec first, double second){
     Vec new;
     new.x = first.x / second;
     new.y = first.y / second;
     return new;
+}
+
+double magnitude(Vec vec){
+    return sqrt((vec.x * vec.x) + (vec.y * vec.y));
+}
+
+Vec unit_vec(Vec first){
+    Vec new;
+
 }
 void print_vec(Vec vec){
     printf("x: %f, y %f\n", vec.x, vec.y);
@@ -38,7 +48,11 @@ int main(){
     Vec newVec;
     newVec.x = 5.0;
     newVec.y = 2.4;
- 
-    Vec z = mul(newVec, 2);
+
+    Vec z = add(mul(newVec, 2), newVec);
+    double y = magnitude(z);
     print_vec(z);
+
+    printf("magnitude: %f\n", y);
+
 }
